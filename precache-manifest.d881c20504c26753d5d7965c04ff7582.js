b87af3d816ea352fca044e1eebf4cf89d193ddf7
Jaylord Tagalan <jaylordtagalan@gmail.com>
2021-12-01 22:18:53 +0800

    Updates

---
+++ precache-manifest.d881c20504c26753d5d7965c04ff7582.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5115093f26ac04cf24101780e5844c8",
+    "url": "/covid19tracker-react/index.html"
+  },
+  {
+    "revision": "93c68bdde358c69f0645",
+    "url": "/covid19tracker-react/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "a8e69edccf0d0a33a75e",
+    "url": "/covid19tracker-react/static/css/main.f0687bff.chunk.css"
+  },
+  {
+    "revision": "93c68bdde358c69f0645",
+    "url": "/covid19tracker-react/static/js/2.6d0d585d.chunk.js"
+  },
+  {
+    "revision": "0b110a718d0a8fc6b16693cbaab585fa",
+    "url": "/covid19tracker-react/static/js/2.6d0d585d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8e69edccf0d0a33a75e",
+    "url": "/covid19tracker-react/static/js/main.7a015e0b.chunk.js"
+  },
+  {
+    "revision": "2ad6425f9d9bdf59a746",
+    "url": "/covid19tracker-react/static/js/runtime-main.669eae66.js"
+  },
+  {
+    "revision": "de92d3f1f38fabd364d1f5730c3536e3",
+    "url": "/covid19tracker-react/static/media/Logo.de92d3f1.png"
+  }
+]);
